ac59be6d5a953cdb9af4b3fafae7ecf4b3ae6d52
Juliano Resende Diniz <juliano.resende@gmail.com>
2026-02-08 13:33:09 -0300

    fixed security issues`

--- config.py
+++ config.py
@@ -2,13 +2,16 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     # Default
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 class ProductionConfig(Config):
     # Debug
     DEBUG = False
+
 
 class DevelopmentConfig(Config):
     # Debug
